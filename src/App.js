7d61de2feae1e7099243aaf660da5425b173e3ec
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-05-01 15:39:08 +0100

    Write handlesubmit method

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ import { SearchBar, VideoDetail } from './components';
 import youtube from './api/youtube'
 
 class App extends React.Component {
+  state = {
+    videos: []
+    selectedVideo: null,
+  }
+
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', { 
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: process.env.REACT_APP_YOUTUBE_API_KEY,
+        q: searchTerm,
+    }
+  });
+
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0]});
+  }
+
   render () {
     return(
-      <Grid justify="center" container spacing={16}>
+      <Grid justify="center" container spacing={10}>
         <Grid item xs={12}>
-          <Grid container spacing={16}>
+          <Grid container spacing={10}>
             <Grid item xs={12}>
-              <SearchBar />
+              <SearchBar onFormSubmit={this.handleSubmit} />
             </Grid>
             <Grid item xs={8}>
               <VideoDetail />
